16cd693f9589771ebf980bb64fedcf10efacd988
Antônio Cruz <antoniocruzgomes880@gmail.com>
2023-07-05 13:17:27 -0300

    mudanca do construtor da classe jogada

--- Trabalho_03TecProg/src/negocio/Jogada.java
+++ Trabalho_03TecProg/src/negocio/Jogada.java
@@ -1,20 +1,20 @@
 package negocio;
 
 public class Jogada {
-	private String jogadorVez;
+	private Jogador jogadorVez;
 	private String fraseDica;
 	private String cartaEscolhida;
 	private int pontuacao;
 	private int id;
 	
 	public Jogada(String jogadorVez, String fraseDica, String cartaEscolhida, int pontuacao) {
-		this.jogadorVez = jogadorVez;
+		this.jogadorVez.setNome(jogadorVez);
 		this.fraseDica = fraseDica;
 		this.cartaEscolhida = cartaEscolhida;
 		this.pontuacao = pontuacao;
 	}
 	public Jogada() {
-		this.jogadorVez = "";
+		this.jogadorVez.setNome("");
 		this.fraseDica = "";
 		this.cartaEscolhida = "";
 		this.pontuacao = 0;
@@ -23,7 +23,7 @@ public class Jogada {
 		return cartaEscolhida;
 	}
 	public String getJogadorVez() {
-		return jogadorVez;
+		return jogadorVez.getNome();
 	}
 	public String getFraseDica() {
 		return fraseDica;
